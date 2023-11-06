1c3c659d2f1d426203350d4d7142f6f4e7630636
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-06 11:50:21 +0100

    add visual check

--- TestInterconnected.jl
+++ TestInterconnected.jl
@@ -1,8 +1,9 @@
 include("ImportExport/model_builder.jl")
 using Gurobi
+using Plots
 
 scenario = "National Trends"
-endtime = 240
+endtime = 24 * 10 
 year = 2025
 CY = 1984
 CY_ts = 2012
@@ -13,63 +14,47 @@ results = DataFrame()
 isolated = false
 country = "BE00"
 
+m = Model(optimizer_with_attributes(Gurobi.Optimizer))
+define_sets!(m,scenario,year,CY,[],[country])
+
+process_parameters!(m,scenario,year,CY,[country])
+process_time_series!(m,scenario,year,CY_ts)
+remove_capacity_country!(m,country)
+build_NTC_investment_model!(m,endtime,VOLL,0.1)
+optimize!(m)
 
 
-for isolated in [true,false]
-    for endtime in [240,480,720,8760]
-        m = Model(optimizer_with_attributes(Gurobi.Optimizer))
-        define_sets!(m,scenario,year,CY,[],[country])
+#Check if production + net_import equals demand 
 
-        if isolated
-            #We start by redefining the sets here, to remove unnecessary countries. 
-            all_countries = [key for key in keys(m.ext[:sets][:technologies])]
-            define_sets!(m,scenario,year,CY,filter((e->e != "BE00"),all_countries),["BE00"])
-            process_parameters!(m,scenario,year,CY,[country])
-            process_time_series!(m,scenario,year,CY_ts)
-            remove_capacity_country!(m,country)
-            build_isolated_investment_model!(m,endtime,VOLL)
-        else 
-            process_parameters!(m,scenario,year,CY,[country])
-            process_time_series!(m,scenario,year,CY_ts)
-            remove_capacity_country!(m,country)
-            build_NTC_investment_model!(m,endtime,VOLL,0.1)
+c_import = [sum(JuMP.value.(m.ext[:variables][:import][country,neighbor,t] for neighbor in m.ext[:sets][:connections][country])) for t in 1:endtime]
+c_export = [sum(JuMP.value.(m.ext[:variables][:export][country,neighbor,t] for neighbor in m.ext[:sets][:connections][country])) for t in 1:endtime]
+demand = [m.ext[:timeseries][:demand][country][t] for t in 1:endtime]
+curtailment = [JuMP.value.(m.ext[:variables][:curtailment][country,t] ) for t in 1:endtime]
+load_shedding = [JuMP.value.(m.ext[:variables][:load_shedding][country,t] ) for t in 1:endtime]
 
-        end
-        optimize!(m)
 
-        # print("Belgium isolated = ",isolated, JuMP.value.(m.ext[:variables][:invested_cap]))
+tech = "OCGT"
+productions = Dict(tech => [JuMP.value.(m.ext[:variables][:production][country,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:investment_technologies][country] )
 
-        demand = sum(m.ext[:timeseries][:demand][country][1:endtime])
-        if isolated 
-            imported = 0
-        else
-            imported = sum([sum(JuMP.value.(m.ext[:variables][:import][country,neighbor,t]
-                for neighbor in m.ext[:sets][:connections][country])) for t in 1:endtime])
-        end
+# Extract x values (time periods)
+x = 1:length(productions["OCGT"])
 
-        row = DataFrame(
-            "scenario" => scenario,
-            "end" => endtime,
-            "year" => year,
-            "CY" => CY,
-            "CY_ts" => CY_ts,
-            "VOLL" => VOLL,
-            "isolated" => isolated,
-            "CO2_cost"=>sum(JuMP.value.(m.ext[:expressions][:CO2_cost][country,tech,t] for tech in m.ext[:sets][:dispatchable_technologies][country] for t in 1:endtime)),
-            "load_shedding_cost"=>sum(JuMP.value.(m.ext[:expressions][:load_shedding_cost][country,t] for t in 1:endtime)),
-            "VOM_cost"=>sum(JuMP.value.(m.ext[:expressions][:VOM_cost][country,tech,t] for tech in m.ext[:sets][:technologies][country] for t in 1:endtime)),
-            "fuel_cost"=>sum(JuMP.value.(m.ext[:expressions][:fuel_cost][country,tech,t] for tech in m.ext[:sets][:dispatchable_technologies][country] for t in 1:endtime)),
-            "investment_cost"=>sum(JuMP.value.(m.ext[:expressions][:investment_cost][country,tech] for tech in m.ext[:sets][:investment_technologies][country])),
-            "CCGT"=> JuMP.value.(m.ext[:variables][:invested_cap][country,"CCGT"]),
-            "OCGT"=> JuMP.value.(m.ext[:variables][:invested_cap][country,"OCGT"]),
-            "PV"=> JuMP.value.(m.ext[:variables][:invested_cap][country,"PV"]),
-            "w_on"=> JuMP.value.(m.ext[:variables][:invested_cap][country,"w_on"]),
-            "w_off"=> JuMP.value.(m.ext[:variables][:invested_cap][country,"w_off"]),
-            "imported" => imported,
-            "demand" => demand
-        )
-        results = vcat(results,row)
+# Initialize a variable to store the stacked production values
+y_stacked = c_import - c_export
+plt = plot(y_stacked,label = "net_import")
 
-        CSV.write("Results\\model_results.csv",results)
-    end
+# Create a stacked line plot
+for tech  in ["PV", "w_on", "w_off", "CCGT", "OCGT"]
+    # Add the production values to the stacked values
+    y_stacked += productions[tech]
+
+    #plot!(x, y_stacked, label = tech)
+    plot!(x,productions[tech], label = tech)
 end
+
+scatter!(x,demand + curtailment - load_shedding - y_stacked,label = "Sum of it all")
+# Customize the plot
+plot!(xlabel = "Time", ylabel = "Production", legend = :topright)
+
+# Display the plot
+display(plt)
