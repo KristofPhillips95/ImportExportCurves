135c4411c2f079412acbe6c68a6845d56b754fdd
KristofPhillips95 <kristofphillips@gmail.com>
2023-11-07 10:53:56 +0100

    update TestInterconnected.jl

--- TestInterconnected.jl
+++ TestInterconnected.jl
@@ -25,17 +25,21 @@ optimize!(m)
 
 
 #Check if production + net_import equals demand 
-country = "DE00"
-c_import = [sum(JuMP.value.(m.ext[:variables][:import][country,neighbor,t] for neighbor in m.ext[:sets][:connections][country])) for t in 1:endtime]
-c_export = [sum(JuMP.value.(m.ext[:variables][:export][country,neighbor,t] for neighbor in m.ext[:sets][:connections][country])) for t in 1:endtime]
-demand = [m.ext[:timeseries][:demand][country][t] for t in 1:endtime]
-curtailment = [JuMP.value.(m.ext[:variables][:curtailment][country,t] ) for t in 1:endtime]
-load_shedding = [JuMP.value.(m.ext[:variables][:load_shedding][country,t] ) for t in 1:endtime]
+country_plot = "BE00"
+plot_country_is_target = country_plot == country
+c_import = [sum(JuMP.value.(m.ext[:variables][:import][country_plot,neighbor,t] for neighbor in m.ext[:sets][:connections][country_plot])) for t in 1:endtime]
+c_export = [sum(JuMP.value.(m.ext[:variables][:export][country_plot,neighbor,t] for neighbor in m.ext[:sets][:connections][country_plot])) for t in 1:endtime]
+demand = [m.ext[:timeseries][:demand][country_plot][t] for t in 1:endtime]
+curtailment = [JuMP.value.(m.ext[:variables][:curtailment][country_plot,t] ) for t in 1:endtime]
+load_shedding = [JuMP.value.(m.ext[:variables][:load_shedding][country_plot,t] ) for t in 1:endtime]
 
-tech = "OCGT"
-productions = Dict(tech => [JuMP.value.(m.ext[:variables][:production][country,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:technologies][country] )
-charges = Dict(tech => [JuMP.value.(m.ext[:variables][:charge][country,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:storage_technologies][country] )
-# Extract x values (time periods)
+if plot_country_is_target
+    productions = Dict(tech => [JuMP.value.(m.ext[:variables][:production][country_plot,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:technologies][country_plot] )
+    charges = Dict(tech => [JuMP.value.(m.ext[:variables][:charge][country_plot,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:storage_technologies][country_plot] )
+else
+    productions = Dict(tech => [JuMP.value.(m.ext[:variables][:production][country_plot,tech,t]) for t in 1:endtime] for tech in m.ext[:sets][:investment_technologies][country_plot] )
+end
+    # Extract x values (time periods)
 x = 1:length(productions["OCGT"])
 
 # Initialize a variable to store the stacked production values
@@ -51,18 +55,26 @@ for tech  in keys(productions)
     #plot!(x, y_stacked, label = tech)
     plot!(x,productions[tech], label = tech)
 end
-charges_stacked = zeros(endtime)
-for tech  in keys(charges)
-    # Add the production values to the stacked values
-    charges_stacked += charges[tech]
+if plot_country_is_target
+    charges_stacked = zeros(endtime)
+    for tech  in keys(charges)
+        # Add the production values to the stacked values
+        charges_stacked += charges[tech]
 
-    #plot!(x, y_stacked, label = tech)
-    plot!(x,charges[tech], label = string("charge" , tech))
+        #plot!(x, y_stacked, label = tech)
+        plot!(x,charges[tech], label = string("charge " , tech))
+    end
 end
-
-scatter!(x,demand + curtailment + charges_stacked - load_shedding - y_stacked,label = "Sum of it all")
+plot!(x,curtailment,label = "curtailment")
+plot!(x,load_shedding,label = "load_shedding")
+if plot_country_is_target
+    scatter!(x,demand + curtailment + charges_stacked - load_shedding - y_stacked,label = "Sum of it all")
+else
+    scatter!(x,demand + curtailment - load_shedding - y_stacked,label = "Sum of it all")
+end
 # Customize the plot
-plot!(xlabel = "Time", ylabel = "Production", legend = :topright)
+plot!(xlabel = "Time", ylabel = "Production", legend = :topright,title = country_plot)
 
 # Display the plot
 display(plt)
+savefig("Results/Figures/SanityCheck/Nodal_balance_$(country)_$(country_plot)")
