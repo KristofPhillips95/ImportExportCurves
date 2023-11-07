e3c279da7cd1f7130b50fb8a53e0f92654430955
KristofPhillips95 <kristofphillips@gmail.com>
2023-11-07 08:35:22 +0100

    simplified model submission

---
+++ sub_loop_Scenarios_simplified.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="ScenarioLoopImExp_simplified"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="12:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+
+cd $VSC_DATA/ImportExportCurves
+
+echo "Starting runs"
+
+julia LoopScenarios_simplified.jl
